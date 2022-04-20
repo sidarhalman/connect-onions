6e3bcf3b4bb5d10f73dc69e2a27032edf88ff02c
sidarhalman <sidarhalman93@gmail.com>
2022-04-20 22:22:45 +0200

    add dotenv morgan body-parser, nodemon

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
 const express = require('express');
-
+const morgan = require('morgan');
+const bodyParser =  require('body-parser');
 const app = express();
 
 const userRouter = require('./routes/userRoutes');
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
 app.use('/users', userRouter);
 

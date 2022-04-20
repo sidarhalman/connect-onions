f5142c7be10a994da6e02a723c6a2b081cc37721
sidarhalman <sidarhalman93@gmail.com>
2022-04-20 17:16:43 +0200

    connect user router to app.js

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => 
-{
-    res.send('Server running');
-});
+const userRouter = require('./routes/userRoutes');
+
+app.use('/users', userRouter);
 
 module.exports = app;

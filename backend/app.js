39bff524ab565a2a081c601c75a718e42b65b8ce
sidarhalman <sidarhalman93@gmail.com>
2022-05-07 14:06:29 +0200

    put local DB name in env

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const router = require('./routes/userRoutes');
 const app = express();
 
 mongoose.connect(
-    "mongodb://localhost:27017/onion",
+    `mongodb://localhost:27017/${process.env.DB_NAME}`,
     {
         useNewUrlParser:true,
         useUnifiedTopology:true

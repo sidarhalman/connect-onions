c5dbc2c465362e952f279175ad0e9c661c04888b
sidarhalman <sidarhalman93@gmail.com>
2022-05-18 14:15:05 +0200

    add background color

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,6 @@
 import React, { useState } from 'react'
-import { Route, Routes} from 'react-router';
-import { Divider } from '@material-ui/core';
 
 import './App.css';
-import ResponsiveAppBar from "./components/Header/Nav";
-import Home from './components/Header/Home';
-import Products from './components/Header/Products';
-import Users from './components/Header/Users';
-import Location from './components/Header/Location';
-import Dashboard from './components/UserSettings/Dashboard';
-import Profile from './components/UserSettings/Profile';
-import Signup from './components/UserSettings/Signup';
-import Searchbar from './components/Header/Searchbar';
-import Questions from './components/Header/Questions';
 
 function App() {
   
